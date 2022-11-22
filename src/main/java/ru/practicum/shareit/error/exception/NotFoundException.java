c3e93d328ba5a47fa4a9613b882ad86439db6fee
Dmitree <color.ivanov2016@yandex.ru>
2022-11-22 20:41:46 +0300

    fix problem

--- src/main/java/ru/practicum/shareit/error/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/error/exception/NotFoundException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.error.exception;
 
-public class NotFoundException extends RuntimeException{
+public class NotFoundException extends RuntimeException {
     public NotFoundException(final String message) {
         super(message);
     }

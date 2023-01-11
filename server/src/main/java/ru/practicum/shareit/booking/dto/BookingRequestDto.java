8b41adcc0d46f873f8c7bf586e3e937c3f98144e
Dmitree <color.ivanov2016@yandex.ru>
2023-01-11 16:26:50 +0300

    add new branch add-docker

---
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingRequestDto.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.booking.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingRequestDto {
+
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Long itemId;
+}
